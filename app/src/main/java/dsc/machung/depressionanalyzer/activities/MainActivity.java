846dc0ed120b3505e868ddb5208d6b9edc4a8489
Fikry Farenza <vickyfarenza@gmail.com>
2021-03-03 05:38:41 +0700

    :tada:

---
+++ app/src/main/java/dsc/machung/depressionanalyzer/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package dsc.machung.depressionanalyzer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dsc.machung.depressionanalyzer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
