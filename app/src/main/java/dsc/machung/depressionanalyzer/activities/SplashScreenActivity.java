dbeebb86766c656c9381c386a7900ddda0d888e9
Fikry Farenza <vickyfarenza@gmail.com>
2021-03-03 12:17:40 +0700

    :construction: Add Splash Screen Activity

---
+++ app/src/main/java/dsc/machung/depressionanalyzer/activities/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package dsc.machung.depressionanalyzer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import dsc.machung.depressionanalyzer.databinding.ActivitySplashScreenBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ActivitySplashScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(
+                        new Intent(SplashScreenActivity.this, MainActivity.class)
+                );
+                finish();
+            }
+        },2000);
+    }
+}
