afc81433c393ee531dd04e4db0c01662b8244398
Fikry Farenza <vickyfarenza@gmail.com>
2021-03-03 12:17:01 +0700

    :tractor: Add viewBinding & update dependencies :arrow_up: :sparkles:

--- app/src/main/java/dsc/machung/depressionanalyzer/activities/MainActivity.java
+++ app/src/main/java/dsc/machung/depressionanalyzer/activities/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import dsc.machung.depressionanalyzer.R;
+import dsc.machung.depressionanalyzer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
