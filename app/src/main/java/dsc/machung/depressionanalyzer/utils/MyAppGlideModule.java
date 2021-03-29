b4486e993680576ad529233384ee565410c0df9f
Fikry Farenza <vickyfarenza@gmail.com>
2021-03-29 21:54:57 +0700

    :arrow_up: Add glide v4

---
+++ app/src/main/java/dsc/machung/depressionanalyzer/utils/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package dsc.machung.depressionanalyzer.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
